5796b8ca6c2854a8bb5612363311132d59c62731
Jan Kreutzfeld <jan.kreutzfeld@gmail.com>
2023-10-15 11:36:39 +0200

    Clean up Pot class

--- src/main/java/org/continuouspoker/dealer/data/Pot.java
+++ src/main/java/org/continuouspoker/dealer/data/Pot.java
@@ -47,39 +47,43 @@ public class Pot implements Serializable {
     }
 
     public void pay(final Map<Score, List<Player>> rankedPlayers) {
-        rankedPlayers.forEach((score, players) -> {
-            final Iterator<PotPart> iterator = pots.iterator();
-            final HashSet<Player> winners = new HashSet<>();
-            while (iterator.hasNext()) {
-                final PotPart pot = iterator.next();
-                final int potSize = pot.getSize();
-                final Collection<Player> payees = pot.getPayees();
-                winners.clear();
-                winners.addAll(players);
-                winners.retainAll(payees);
-                if (!winners.isEmpty()) {
-                    final int split = potSize / winners.size();
-                    winners.forEach(p -> p.addToStack(split));
-
-                    final String winnerString = winners.stream().map(Player::getName).collect(Collectors.joining(","));
-                    log.info("Winners: {} ({} each) with a {}", winnerString, split, score.name());
-
-                    if (moreThanOneWinner(winners)) {
-                        stepLogger.log(
-                                String.format("%s of %s is split between %s (%s for each), for a '%s'", pot.getName(),
-                                        potSize, winnerString, split, score.name()));
-                    } else {
-                        stepLogger.log(
-                                String.format("%s of %s goes to %s, for a '%s'", pot.getName(), potSize, winnerString,
-                                        score.name()));
-                    }
-
-                    iterator.remove();
-                }
-            }
-        });
-
+        rankedPlayers.forEach(this::payRankedPlayers);
         reset();
+    }
+
+    private void payRankedPlayers(final Score score, final List<Player> players) {
+        final Iterator<PotPart> iterator = pots.iterator();
+        final HashSet<Player> winners = new HashSet<>();
+        while (iterator.hasNext()) {
+            final PotPart pot = iterator.next();
+            final Collection<Player> payees = pot.getPayees();
+            winners.clear();
+            winners.addAll(players);
+            winners.retainAll(payees);
+            if (!winners.isEmpty()) {
+                payPotToWinners(score, winners, pot);
+                iterator.remove();
+            }
+        }
+    }
+
+    private void payPotToWinners(final Score score, final HashSet<Player> winners, final PotPart pot) {
+        final int potSize = pot.getSize();
+        final int split = potSize / winners.size();
+        winners.forEach(p -> p.addToStack(split));
+
+        final String winnerString = winners.stream().map(Player::getName).collect(Collectors.joining(","));
+        log.info("Winners: {} ({} each) with a {}", winnerString, split, score.name());
+
+        if (moreThanOneWinner(winners)) {
+            stepLogger.log(
+                    String.format("%s of %s is split between %s (%s for each), for a '%s'", pot.getName(), potSize,
+                            winnerString, split, score.name()));
+        } else {
+            stepLogger.log(String.format("%s of %s goes to %s, for a '%s'", pot.getName(), potSize, winnerString,
+                    score.name()));
+        }
+
     }
 
     private static boolean moreThanOneWinner(final Collection<Player> winners) {
