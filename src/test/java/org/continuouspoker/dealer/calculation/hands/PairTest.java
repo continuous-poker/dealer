7d4a1bcb29d680b61d838a35d8306457b27ab843
apena <andres.pena@doubleslash.de>
2024-01-22 11:07:58 +0100

    Update copyright header in source and all files containing source code.

--- src/test/java/org/continuouspoker/dealer/calculation/hands/PairTest.java
+++ src/test/java/org/continuouspoker/dealer/calculation/hands/PairTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024 DoubleSlash Net-Business GmbH
+ * Copyright © 2020 - 2024 Jan Kreutzfeld
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
