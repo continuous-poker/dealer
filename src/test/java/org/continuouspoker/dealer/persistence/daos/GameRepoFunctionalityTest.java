95866534a81bc2fdefc623b90a0adbd70f590337
apena <andres.pena@doubleslash.de>
2024-01-26 15:37:59 +0100

    Create test classes for both DAOs.

---
+++ src/test/java/org/continuouspoker/dealer/persistence/daos/GameRepoFunctionalityTest.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright © 2024 DoubleSlash Net-Business GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.continuouspoker.dealer.persistence.daos;
+
+import static io.smallrye.common.constraint.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import io.quarkus.panache.common.Sort;
+import io.quarkus.test.TestTransaction;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.h2.H2DatabaseTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.continuouspoker.dealer.RemotePlayer;
+import org.continuouspoker.dealer.Team;
+import org.continuouspoker.dealer.game.Game;
+import org.continuouspoker.dealer.persistence.entities.GameBE;
+import org.continuouspoker.dealer.persistence.entities.ScoreRecordBE;
+import org.continuouspoker.dealer.persistence.entities.TeamBE;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+@QuarkusTestResource(H2DatabaseTestResource.class)
+@TestTransaction
+class GameRepoFunctionalityTest {
+    GameDAO testee = new GameDAO();
+    LogEntryDAO logDao = new LogEntryDAO();
+    Game game1;
+    Game game2;
+
+    @BeforeEach
+    void setup() {
+        game1 = new Game(0L, "game_test_1", Duration.parse("PT1S"), Duration.parse("PT1S"), testee, logDao);
+        game2 = new Game(0L, "game_test_2", Duration.parse("PT1S"), Duration.parse("PT1S"), testee, logDao);
+    }
+
+    @Test
+    void givenGameInstance_whenCreateGameMethodIsCalled_thenStoreInDatabase() {
+        // Arrange
+        testee.createGame(game1);
+
+        PanacheQuery<GameBE> result = GameBE.findAll();
+        List<GameBE> games = result.list();
+
+        assertNotNull(result);
+        assertEquals(game1.getName(), games.get(0).getName());
+    }
+
+    @Test
+    void givenListOfGameInstances_whenCreateMethodIsCalled_thenStoreAllInDatabase() {
+        // Arrange
+        Set<Game> games = Set.of(game1, game2);
+
+        testee.storeGames(games);
+
+        PanacheQuery<GameBE> result = GameBE.findAll(Sort.by("name", Sort.Direction.Ascending));
+        List<GameBE> gameList = result.list();
+
+        // Assert
+        assertTrue(!gameList.isEmpty());
+        assertEquals("game_test_1", gameList.get(0).getName());
+        assertEquals("game_test_2", gameList.get(1).getName());
+    }
+
+    @Test
+    void whenLoadGamesMethodIsCalled_thenReturnGames() {
+        // Arrange
+        testee.createGame(game1);
+
+        Optional<List<Game>> result = testee.loadGames();
+
+        assertNotNull(result);
+        assertTrue(result.isPresent());
+
+        Game g = result.get().get(0);
+        assertEquals("game_test_1", g.getName());
+    }
+
+    @Test
+    void givenTeamInstance_whenCreateTeamMethodIsCalled_thenStoreInDatabase() {
+        // Arrange
+        testee.createTeam("team_test", "http://localhost:8081");
+
+        PanacheQuery<TeamBE> result = TeamBE.findAll();
+        List<TeamBE> teams = result.list();
+
+        assertNotNull(result);
+        assertEquals("team_test", teams.get(0).getName());
+        assertEquals("http://localhost:8081", teams.get(0).getProviderUrl());
+    }
+
+    @Test
+    void givenGameInstance_whenStoreScoreMethodIsCalled_thenAddScoreAndPersist() {
+        Game game = mockGameClass();
+
+        testee.storeScores(game);
+
+        PanacheQuery<ScoreRecordBE> result = ScoreRecordBE.findAll();
+        List<ScoreRecordBE> scores = result.list();
+
+        assertNotNull(result);
+        assertEquals(2L, scores.get(0).getGameId());
+    }
+
+    @Test
+    void givenAGameId_whenLoadScoresIsCalled_thenReturnScores() {
+        // Arrange
+        Game game = mockGameClass();
+        testee.storeScores(game);
+
+        Optional<List<ScoreRecordBE>> opt = testee.loadScores(game.getGameId());
+        List<ScoreRecordBE> scores = opt.orElse(null);
+        assertNotNull(scores);
+
+        ScoreRecordBE score = scores.get(0);
+        assertEquals(2L, score.getGameId());
+    }
+
+    private Game mockGameClass() {
+        Team team = mock(Team.class);
+        when(team.getScore()).thenReturn(10L);
+        when(team.getProvider()).thenReturn(new RemotePlayer("http://localhost:8081"));
+
+        Game game = mock(Game.class);
+        when(game.getGameId()).thenReturn(2L);
+        when(game.getTeams()).thenReturn(List.of(team));
+
+        return game;
+    }
+}
---
+++ src/test/java/org/continuouspoker/dealer/persistence/daos/LogEntryRepoFunctionalityTest.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright © 2024 DoubleSlash Net-Business GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.continuouspoker.dealer.persistence.daos;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import io.quarkus.panache.common.Sort;
+import io.quarkus.test.TestTransaction;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.h2.H2DatabaseTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.continuouspoker.dealer.LogEntry;
+import org.continuouspoker.dealer.persistence.entities.LogEntryBE;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import io.quarkus.panache.mock.PanacheMock;
+import org.mockito.InjectMocks;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@QuarkusTest
+@QuarkusTestResource(H2DatabaseTestResource.class)
+@TestTransaction
+class LogEntryRepoFunctionalityTest {
+
+    LogEntryDAO testee = new LogEntryDAO();
+
+    @Test
+    void givenAListOfLogEntries_whenStoreLogEntriesIsCalled_thenPersist() {
+        testee.storeLogEntries(mockLogEntries());
+
+        PanacheQuery<PanacheEntityBase> query = LogEntryBE.find("gameId", 1L);
+        List<LogEntryBE> logs = query.list();
+
+        assertNotNull(query);
+        assertEquals(1, logs.size());
+    }
+
+    @Disabled
+    @Test
+    void givenAGameIdAndTimeStamp_whenFindLogsSinceMethodIsCalled_thenReturnLogEntries() {
+//        LogEntry log1 = mock(LogEntry.class);
+        ZonedDateTime currentTimestamp = ZonedDateTime.now();
+//        when(log1.getTimestamp()).thenReturn(currentTimestamp);
+//        when(log1.getGameId()).thenReturn(1L);
+//
+//        testee.storeLogEntries(List.of(log1));
+
+//        ZonedDateTime date = (ZonedDateTime.of(
+//                LocalDateTime.of(2024, 01, 01, 00,00,00),
+//                ZoneId.systemDefault())); // 2024-01-01T00:00:00
+//        List<LogEntry> result = testee.findLogsSince(1L, currentTimestamp.toString());
+//
+//        assertNotNull(result);
+//        assertEquals(1, result.size());
+//        assertEquals(1L, result.get(0).getGameId());
+//        assertEquals(currentTimestamp, result.get(0).getTimestamp());
+
+        // Arrange
+        LogEntry log = new LogEntry(ZonedDateTime.now(), 1L, 2L, 3L, "");
+
+        testee.storeLogEntries(List.of(log));
+
+        ZonedDateTime date = (ZonedDateTime.of(
+                                LocalDateTime.of(2024, 01, 01, 00,00,00),
+                                ZoneId.systemDefault())); // 2024-01-01T00:00:00
+        List<LogEntry> result = testee.findLogsSince(1L, date.toString());
+
+        assertEquals(1, result.size());
+        assertEquals(1L, result.get(0).getGameId());
+    }
+
+    @Test
+    void givenAGameId_whenFindLogsByGameIdMethodIsCalled_thenOnlyReturnOneLog() {
+        LogEntry log = mock(LogEntry.class);
+        LogEntry falseLog = mock(LogEntry.class);
+        when(log.getGameId()).thenReturn(10L);
+        when(falseLog.getGameId()).thenReturn(5L);
+
+        testee.storeLogEntries(List.of(log, falseLog));
+
+        List<LogEntry> result = testee.findLogsByGameId(10L);
+
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals(10L, result.get(0).getGameId());
+    }
+
+    @Test
+    void shouldReturnEmptyList_whenFindLogsByGameIdMethodIsCalled() {
+        testee.storeLogEntries(mockLogEntries());
+
+        List<LogEntry> result = testee.findLogsByGameId(0L);
+
+        assertNotNull(result);
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void givenATournamentId_whenFindLogsByTournamentId_thenReturnLogEntries() {
+        testee.storeLogEntries(mockLogEntries());
+
+        List<LogEntry> result = testee.findLogsByTournamentId(2L);
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    void shouldReturnEmptyList_whenFindLogsByTournamentIdMethodIsCalled() {
+        LogEntry log = mock(LogEntry.class);
+        when(log.getTournamentId()).thenReturn(5L);
+
+        testee.storeLogEntries(List.of(log));
+
+        List<LogEntry> result = testee.findLogsByTournamentId(6L);
+
+        assertNotNull(result);
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void givenARoundId_whenFindLogsByRoundId_thenReturnLogEntries() {
+        testee.storeLogEntries(mockLogEntries());
+
+        List<LogEntry> result = testee.findLogsByRoundId(3L);
+
+        assertNotNull(result);
+        assertEquals(1, result.size());
+    }
+
+    private List<LogEntry> mockLogEntries() {
+        LogEntry log1 = mock(LogEntry.class);
+        when(log1.getGameId()).thenReturn(1L);
+        when(log1.getTournamentId()).thenReturn(2L);
+        when(log1.getRoundId()).thenReturn(3L);
+
+        LogEntry log2 = mock(LogEntry.class);
+        when(log2.getGameId()).thenReturn(10L);
+        when(log2.getTournamentId()).thenReturn(2L);
+        when(log2.getRoundId()).thenReturn(5L);
+        return List.of(log1, log2);
+    }
+
+
+}
