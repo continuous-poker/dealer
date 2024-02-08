b8ae585815b75fa5ccc29842c1fa1228c989560a
apena <andres.pena@doubleslash.de>
2024-02-08 12:41:03 +0100

    Add copyright header.

--- src/test/java/org/continuouspoker/dealer/persistence/mappers/GameMapperTest.java
+++ src/test/java/org/continuouspoker/dealer/persistence/mappers/GameMapperTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2020-2024 doubleSlash Net-Business GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.continuouspoker.dealer.persistence.mappers;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -12,8 +28,6 @@ import java.util.List;
 import org.continuouspoker.dealer.RemotePlayer;
 import org.continuouspoker.dealer.Team;
 import org.continuouspoker.dealer.game.Game;
-import org.continuouspoker.dealer.persistence.daos.GameDAO;
-import org.continuouspoker.dealer.persistence.daos.LogEntryDAO;
 import org.continuouspoker.dealer.persistence.entities.GameBE;
 import org.continuouspoker.dealer.persistence.entities.TeamBE;
 import org.junit.jupiter.api.Test;
@@ -41,8 +55,6 @@ class GameMapperTest {
     @Test
     void testModelToEntity() {
         // Mocks
-        GameDAO gameDAO = mock(GameDAO.class);
-        LogEntryDAO logEntryDAO = mock(LogEntryDAO.class);
         Team team = mock(Team.class);
         RemotePlayer player = mock(RemotePlayer.class);
 
@@ -51,7 +63,7 @@ class GameMapperTest {
         when(team.getProvider()).thenReturn(player);
         when(team.getProvider().getUrl()).thenReturn("http://remotePlayer");
 
-        Game game = new Game(0L, "gameName", Duration.parse("PT1S"), Duration.parse("PT1S"), gameDAO, logEntryDAO);
+        Game game = new Game(0L, "gameName", Duration.parse("PT1S"), Duration.parse("PT1S"));
         game.addPlayer(team);
 
         GameBE gameBE = testee.toEntity(game);
